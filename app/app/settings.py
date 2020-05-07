af52775083fb57e1d8d812621450c2ad5b210b7b
Oyshik Moitra <oyshikmoitra@hotmail.com>
2020-05-07 17:42:05 +0530

    Added Auth Token APIs

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cw&42+&z2-km6rzxjhem^=^ted3)im5&lcs6h_rw!)%le2(sa^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.39.0.16', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
